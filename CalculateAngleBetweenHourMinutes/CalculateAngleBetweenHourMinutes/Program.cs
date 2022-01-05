8e16435b3cefb89515f868fcba4a1bc17a7d1fa5
Rohul Amin Mohammad Dipu <41869272+radipu@users.noreply.github.com>
2022-01-06 02:13:12 +0600

    first commit

---
+++ CalculateAngleBetweenHourMinutes/CalculateAngleBetweenHourMinutes/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace CalculateAngleBetweenHourMinutes
+{
+    public static class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter the Hours: ");
+            int h = int.Parse(Console.ReadLine());
+            Console.WriteLine("Enter the Minutes: ");
+            int m = int.Parse(Console.ReadLine());
+
+            double hourInDegree = (h * 30) + (m * 30.0/60);
+            double minuteInDegree = (m * 6);
+
+            double diff = Math.Abs(hourInDegree - minuteInDegree);
+
+            if (diff > 180)
+                diff = 360 - diff;
+
+            Console.WriteLine($"Angle between {h} hours and {m} minutes is {diff} degree.");
+            Console.ReadKey();
+        }
+    }
+}
